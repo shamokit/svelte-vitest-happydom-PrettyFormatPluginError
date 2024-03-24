731726215af99f3f36d4634c5bb24fe4e4cfd924
shamokit <contact@shamokit.com>
2024-03-24 13:28:00 +0900

    first commit

---
+++ src/lib/Greeter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let name: string;
+	export let onclick: () => void;
+</script>
+
+<ul>
+	<li>
+		<button type="button" on:click="{onclick}">
+			{#if name}
+				<span>{name}</span>
+			{:else}
+				<span> No <span>Name</span> </span>
+			{/if}
+		</button>
+	</li>
+</ul>
+
+<a href="/">{name || 'No Name'}</a>
---
+++ src/lib/Greeter.test.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import Greeter from './Greeter.svelte';
+	import { writable } from 'svelte/store';
+
+	export let name: ComponentProps<Greeter>['name'];
+	let nameStore = writable(name);
+</script>
+
+<Greeter name="{$nameStore}" onclick="{() => nameStore.set('Renamed')}" />
